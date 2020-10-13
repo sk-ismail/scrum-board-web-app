1d0c592dc79c52113096e2855f066523dbdf46da
irfan738 <skismail23@yahoo.com>
2020-10-13 20:01:45 +0530

    Updates

---
+++ precache-manifest.28e81b56dab31e0ba50f41a5f2572ac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fed0e7cd472abb9aded2e994b6126bd",
+    "url": "/scrum-board-web-app/index.html"
+  },
+  {
+    "revision": "9ab428937bf9a63ed3d3",
+    "url": "/scrum-board-web-app/static/css/main.a4b2f9c2.chunk.css"
+  },
+  {
+    "revision": "6f0e48a4a5c79d4fbaf2",
+    "url": "/scrum-board-web-app/static/js/2.16bb9620.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/scrum-board-web-app/static/js/2.16bb9620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab428937bf9a63ed3d3",
+    "url": "/scrum-board-web-app/static/js/main.5cdd24f8.chunk.js"
+  },
+  {
+    "revision": "478a307fbedf8b3ed09c",
+    "url": "/scrum-board-web-app/static/js/runtime-main.fd9efde8.js"
+  },
+  {
+    "revision": "4fa703a8234c187a81d96e8e93323a5f",
+    "url": "/scrum-board-web-app/static/media/alexis-fauvet-u96-HA3rlv8-unsplash.4fa703a8.jpg"
+  }
+]);
